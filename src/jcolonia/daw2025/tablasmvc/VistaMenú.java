b9d21e34848c26ec8d41cc4901ce525b130ddaf8
Héctor Menéndez <hector.mensas@ED09012096P395.educa.jcyl.es>
2026-03-12 09:16:50 -0300

    Clase encargada de formatear y mostrar el menú de opciones al usuario.

--- src/jcolonia/daw2025/tablasmvc/VistaMenú.java
+++ src/jcolonia/daw2025/tablasmvc/VistaMenú.java
@@ -1,17 +1,29 @@
 package jcolonia.daw2025.tablasmvc;
 
 /**
- * Especialización de la vista para gestionar menús de opciones.
+ * Gestión de menús de opciones por consola.
+ * Extiende a VistaGeneral para aprovechar las utilidades de lectura/escritura.
  */
 public class VistaMenú extends VistaGeneral {
+	/** Título que se muestra sobre las opciones. */
     private String título;
+    /** Array con los textos de cada opción disponible. */
     private String[] opciones;
-
+    
+    /**
+     * Crea un menú nuevo con un título y una lista de opciones.
+     * @param título El nombre del menú (ej: "Menú Principal")
+     * @param opciones Array de strings con las descripciones de las opciones.
+     */
     public VistaMenú(String título, String[] opciones) {
         this.título = título;
         this.opciones = opciones;
     }
-
+    
+    /**
+     * Muestra el título y la lista numerada de opciones por pantalla.
+     * Automáticamente añade la opción '0' para salir.
+     */
     public void mostrarOpciones() {
         mostrarTítulo1(título);
         for (int i = 0; i < opciones.length; i++) {
@@ -19,7 +31,11 @@ public class VistaMenú extends VistaGeneral {
         }
         System.out.println("0) Salir");
     }
-
+    
+    /**
+     * Solicita al usuario que introduzca el número de la opción elegida.
+     * @return El número entero introducido por el usuario.
+     */
     public int pedirOpción() {
         return pedirNúmero("Elija una opción");
     }
